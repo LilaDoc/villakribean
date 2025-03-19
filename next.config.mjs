6008ed3acf86834bac43be04d0c6a42c822d5a40
LYJ <liladocdev@gmail.com>
2025-03-19 10:41:42 +0100

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',  // Active l'export statique
+  basePath: process.env.NODE_ENV === "production" ? "/villakribean" : "", // Ajoutez le nom de votre dépôt GitHub
+  images: {
+    unoptimized: true, // Nécessaire pour GitHub Pages
+  },
+};
 
 export default nextConfig;
